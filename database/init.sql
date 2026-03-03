51af76e29182429a9c9e6f31820abec9c6c40dce
Sebastian Lema <sebasjhonlemamontoya12@gmail.com>
2026-03-03 18:53:54 -0500

    Agregar script inicial para la creación de la base de datos y configuraciones

---
+++ database/init.sql
@@ -0,0 +1,7 @@
+-- TODO: Crear la base de datos y configuraciones iniciales
+
+-- Crear extensiones necesarias
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Configurar zona horaria
+SET timezone = 'America/Bogota';
